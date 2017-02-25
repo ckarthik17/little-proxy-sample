d300d05da7dd5bd33f5598e4f93e7fbc67ea0950
Karthik Chandraraj <ckarthik17@gmail.com>
2017-02-25 23:17:52 +0800

    [CK] Chained proxy

--- src/main/java/com/ck/proxy/App.java
+++ src/main/java/com/ck/proxy/App.java
@@ -1,28 +1,47 @@
 package com.ck.proxy;
 
-import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.http.HttpObject;
 import io.netty.handler.codec.http.HttpRequest;
 import io.netty.handler.codec.http.HttpResponse;
-import lombok.extern.slf4j.Slf4j;
 import org.littleshoot.proxy.*;
 import org.littleshoot.proxy.impl.DefaultHttpProxyServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.net.InetSocketAddress;
+import java.util.Queue;
 
-@Slf4j
 public class App {
   private static int PORT = 9080;
+  private static Logger log;
 
   public static void main(String[] args) {
-    HttpFiltersSource filtersSource = getFiltersSource();
+    System.setProperty(org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "INFO");
+    log = LoggerFactory.getLogger(App.class);
 
-    HttpProxyServerBootstrap server = DefaultHttpProxyServer.bootstrap()
+    DefaultHttpProxyServer.bootstrap()
       .withPort(PORT)
       .withAllowLocalOnly(false)
-      .withFiltersSource(filtersSource);
+      .withFiltersSource(getFiltersSource())
+      .withChainProxyManager(getChainedProxyManager())
+      .withTransparent(true)
+      .start();
+  }
 
-    server.start();
+  private static ChainedProxyManager getChainedProxyManager() {
+
+    final ChainedProxyAdapter adapter = new ChainedProxyAdapter() {
+      @Override
+      public InetSocketAddress getChainedProxyAddress() {
+        return new InetSocketAddress("45.64.156.204", 8080);
+      }
+    };
+
+    return new ChainedProxyManager() {
+      public void lookupChainedProxies(HttpRequest httpRequest, Queue<ChainedProxy> chainedProxies) {
+        chainedProxies.add(adapter);
+      }
+    };
   }
 
   private static HttpFiltersSource getFiltersSource() {
@@ -31,51 +50,14 @@ public class App {
       public HttpFilters filterRequest(HttpRequest originalRequest) {
         return new HttpFiltersAdapter(originalRequest) {
 
+          public HttpResponse clientToProxyRequest(HttpObject httpObject) {
+            log.info("clientToProxyRequest");
+            return null;
+          }
+
           public InetSocketAddress proxyToServerResolutionStarted(String resolvingServerHostAndPort) {
-            log.info("Filter: p2SResolutionStarted for: " + resolvingServerHostAndPort);
-            log.info("Filter: originalRequest: " + originalRequest.getUri());
-
-            if (originalRequest.getUri().contains("node1")) {
-              log.info("Routing to server 1");
-              return new InetSocketAddress("210.212.230.22", 80);
-            } else {
-              log.info("Routing to server 2");
-              return new InetSocketAddress("182.73.100.147", 8080);
-            }
-          }
-
-          public void serverToProxyResponseTimedOut() {
-            log.info("serverToProxyResponseTimedOut");
-          }
-
-          public void serverToProxyResponseReceiving() {
-            log.info("serverToProxyResponseReceiving");
-
-          }
-
-          public void serverToProxyResponseReceived() {
-            log.info("serverToProxyResponseReceived");
-          }
-
-          public HttpObject serverToProxyResponse(HttpObject httpObject) {
-            log.info("serverToProxyResponse");
-            return httpObject;
-          }
-
-          public void proxyToServerResolutionSucceeded(String serverHostAndPort, InetSocketAddress resolvedRemoteAddress) {
-            log.info("proxyToServerResolutionSucceeded");
-          }
-          
-          public void proxyToServerResolutionFailed(String hostAndPort) {
-            log.info("proxyToServerResolutionFailed");
-          }
-
-          public void proxyToServerRequestSent() {
-            log.info("proxyToServerRequestSent");
-          }
-
-          public void proxyToServerRequestSending() {
-            log.info("proxyToServerRequestSending");
+            log.info("proxyToServerResolutionStarted");
+            return null;
           }
 
           public HttpResponse proxyToServerRequest(HttpObject httpObject) {
@@ -83,34 +65,14 @@ public class App {
             return null;
           }
 
-          public void proxyToServerConnectionSucceeded(ChannelHandlerContext serverCtx) {
-            log.info("proxyToServerConnectionSucceeded");
-          }
-
-          public void proxyToServerConnectionStarted() {
-            log.info("proxyToServerConnectionStarted");
-          }
-
-          public void proxyToServerConnectionSSLHandshakeStarted() {
-            log.info("proxyToServerConnectionSSLHandshakeStarted");
-          }
-
-          public void proxyToServerConnectionQueued() {
-            log.info("proxyToServerConnectionQueued");
-          }
-
-          public void proxyToServerConnectionFailed() {
-            log.info("proxyToServerConnectionFailed");
+          public HttpObject serverToProxyResponse(HttpObject httpObject) {
+            log.info("serverToProxyResponse");
+            return httpObject;
           }
 
           public HttpObject proxyToClientResponse(HttpObject httpObject) {
             log.info("proxyToClientResponse");
             return httpObject;
-          }
-
-          public HttpResponse clientToProxyRequest(HttpObject httpObject) {
-            log.info("clientToProxyRequest");
-            return null;
           }
         };
       }
